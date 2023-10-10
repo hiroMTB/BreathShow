64a22ffc00586337917e2f14d9109c3658cbbd24
Hiroshi Matoba <matobahiroshi@gmail.com>
2023-10-10 13:01:45 +0200

    move code to cpp

--- BeathShowApp/src/Fan.cpp
+++ BeathShowApp/src/Fan.cpp
@@ -7,3 +7,53 @@
 
 #include "Fan.h"
 
+Fan::Fan(){
+    setup(180);
+    
+    //bool ok = img.load("./tex/test.png");
+    bool ok = img.load("./tex/test2.jpg");
+    if(ok){
+        ofLogNotice("Fan") << "OK loading texture";
+    }else{
+        ofLogError("Fan") << "Can not load texture";
+    }
+};
+
+void Fan::setup(float angle, float len, int direction, int res){
+    
+    mesh.clear();
+    
+    float dA = direction * angle / (float)res;
+    float start = ofDegToRad(-90);
+    for(int i=0; i<res; i++){
+        
+        float a1 = start + ofDegToRad((i + 0) * dA);
+        float a2 = start + ofDegToRad((i + 1) * dA);
+        mesh.addVertex( vec3(0) );
+        mesh.addVertex( vec3(len * cos(a1), len * sin(a1), 0) );
+        mesh.addVertex( vec3(len * cos(a2), len * sin(a2), 0) );
+        
+        float ty = 890.0 * (openAngle/180.0) / res;
+        mesh.addTexCoord( vec2(0, i*ty) );
+        mesh.addTexCoord( vec2(192, i*ty) );
+        mesh.addTexCoord( vec2(192, (i+1)*ty) );
+        
+        mesh.setMode(OF_PRIMITIVE_TRIANGLES );
+    }
+}
+
+void Fan::update(){
+    setup(openAngle, length, direction, resolution);
+}
+
+void Fan::draw(){
+    ofPushMatrix();
+    ofTranslate(position);
+    //ofTranslate(scale);
+    //mesh.drawWireframe();
+    ofTexture & tex = img.getTexture();
+    tex.bind();
+    mesh.draw();
+    tex.unbind();
+    ofPopMatrix();
+}
--- BeathShowApp/src/Fan.h
+++ BeathShowApp/src/Fan.h
@@ -17,56 +17,10 @@ using glm::vec3;
 class Fan : public Saveable{
     
     public:
-    Fan(){
-        setup(180);
-        
-        //bool ok = img.load("./tex/test.png");
-        bool ok = img.load("./tex/test2.jpg");
-        if(ok){
-            ofLogNotice("Fan") << "OK loading texture";
-        }else{
-            ofLogError("Fan") << "Can not load texture";
-        }
-    };
-    
-    void setup(float angle, float len=30, int direction=1, int res = 36){
-
-        mesh.clear();
-        
-        float dA = direction * angle / (float)res;
-        float start = ofDegToRad(-90);
-        for(int i=0; i<res; i++){
-            
-            float a1 = start + ofDegToRad((i + 0) * dA);
-            float a2 = start + ofDegToRad((i + 1) * dA);
-            mesh.addVertex( vec3(0) );
-            mesh.addVertex( vec3(len * cos(a1), len * sin(a1), 0) );
-            mesh.addVertex( vec3(len * cos(a2), len * sin(a2), 0) );
-            
-            float ty = 890.0 * (openAngle/180.0) / res;
-            mesh.addTexCoord( vec2(0, i*ty) );
-            mesh.addTexCoord( vec2(192, i*ty) );
-            mesh.addTexCoord( vec2(192, (i+1)*ty) );
-                        
-            mesh.setMode(OF_PRIMITIVE_TRIANGLES );
-        }
-    }
-    
-    void update(){
-        setup(openAngle, length, direction, resolution);
-    }
-    
-    void draw(){
-        ofPushMatrix();
-        ofTranslate(position);
-        //ofTranslate(scale);
-        //mesh.drawWireframe();
-        ofTexture & tex = img.getTexture();
-        tex.bind();
-        mesh.draw();
-        tex.unbind();
-        ofPopMatrix();
-    }
+    Fan();    
+    void setup(float angle, float len=30, int direction=1, int res = 36);
+    void update();
+    void draw();
     
     ofParameterGroup & getParametersRef() override { return grp; }
 
