68f9fad3d5d1f0e2b31e37a3daa3b7bc0ef7a07f
matoba hiroshi <matobahiroshi@gmail.com>
2023-11-11 14:56:25 +0100

    add rect screen

---
+++ BreathShowApp/src/RectScreen.cpp
@@ -0,0 +1,74 @@
+//
+//  RectScreen.cpp
+//
+
+#include "RectScreen.h"
+
+RectScreen::RectScreen(){
+    
+    setup(size.get().x, size.get().y);
+    
+    bool ok = img.load("./tex/test2.jpg");
+    if(ok){
+        ofLogNotice("Rect") << "OK loading texture";
+    }else{
+        ofLogError("Rect") << "Can not load texture";
+    }
+}
+
+
+void RectScreen::setup(float w, int h){
+    plane.set( w, h, 2, 2, OF_PRIMITIVE_TRIANGLE_STRIP );
+}
+
+
+void RectScreen::loadVideo(string path){
+
+    if( ofFile::doesFileExist(path)){
+        vid.load(path);
+        vid.setUseTexture(true);
+        vid.setLoopState(OF_LOOP_NORMAL);
+        vid.play();
+        ofLogNotice("RectScreen") << "Video file loaded: ";
+    }else{
+        ofLogError("RectScreen") << "Video file does not exist: " << path;
+    }
+}
+
+void RectScreen::update(){
+    if(bOn){
+        setup(size.get().x, size.get().y); // TODO: Stop calling this
+        vid.update();
+    }
+}
+
+void RectScreen::draw(){
+    if(bOn){
+        
+        ofPushMatrix();
+        ofTranslate(position);
+        //ofTranslate(scale);
+        if(bShowTest){
+            plane.mapTexCoords(0, 0, img.getWidth(), img.getHeight());
+            const ofMesh & mesh = plane.getMesh();
+            ofTexture & tex = img.getTexture();
+            tex.bind();
+            mesh.draw();
+            tex.unbind();
+        }else{
+            if(vid.isLoaded()){
+                ofTexture & tex = vid.getTexture();
+                const ofMesh & mesh = plane.getMesh();
+                plane.mapTexCoords(0, 0, 1, 1);
+                tex.bind();
+                mesh.draw();
+                tex.unbind();
+            }
+        }
+        ofPopMatrix();
+    }
+}
+
+void RectScreen::setPlayVideo(bool b){
+    vid.setPaused(b);
+}
---
+++ BreathShowApp/src/RectScreen.h
@@ -0,0 +1,40 @@
+//
+//  RectScreen.h
+//
+
+#ifndef RectScreen_h
+#define RectScreen_h
+
+#include "ofMain.h"
+#include "Saveable.h"
+
+using glm::vec2;
+using glm::vec3;
+
+class RectScreen : public Saveable{
+    
+public:
+    RectScreen();
+    
+    void setup(float w=10, int h=10);
+    void update();
+    void draw();
+    void loadVideo(string path);
+    void setPlayVideo(bool b);
+    
+    ofParameter<bool> bOn{"On", true};
+    ofParameter<bool> bShowTest{"test texture", true};
+    ofParameter<vec3> position{"position", vec3(0,80,0), vec3(-100), vec3(300)};
+    ofParameter<vec2> size{"size", vec2(200), vec2(10), vec2(300)};
+    ofParameter<float> orientationY{"orientationY", 0, -180, 180};
+
+    ofParameterGroup grp{"Rect", bOn, bShowTest, position, size, orientationY};
+    ofParameterGroup & getParametersRef() override { return grp; }
+
+private:
+    ofPlanePrimitive plane;
+    ofImage img;
+    ofVideoPlayer vid;
+};
+
+#endif /* RectScreen_h */
