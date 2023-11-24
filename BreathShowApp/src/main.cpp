be67314d822cc8d86ec6924710b813cd7d617584
matoba hiroshi <matobahiroshi@gmail.com>
2023-11-24 11:20:21 +0100

    remove unused

--- BreathShowApp/src/main.cpp
+++ BreathShowApp/src/main.cpp
@@ -17,30 +17,6 @@ int main( ){
 
     ofSetEscapeQuitsApp(false);
 
-    if(0)
-    {
-        ofGLFWWindowSettings mainWinSettings;
-        mainWinSettings.setGLVersion(4, 1);
-        mainWinSettings.setSize(1280, 720);
-        mainWinSettings.resizable = true;
-        mainWinSettings.decorated = true;
-        shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(mainWinSettings);
-
-        // create a separate window for imGui
-        ofGLFWWindowSettings prjWinSettings;
-        prjWinSettings.setGLVersion(4, 1);
-        prjWinSettings.setSize(1280, 720);
-        prjWinSettings.resizable = true;
-        prjWinSettings.decorated = true;
-        prjWinSettings.shareContextWith = mainWindow;
-        shared_ptr<ofAppBaseWindow> prjWindow = ofCreateWindow(prjWinSettings);
-        
-        shared_ptr<ofApp> mainApp(new ofApp);
-        ofAddListener(prjWindow->events().draw, mainApp.get(), &ofApp::drawProjector);
-        
-        ofRunApp(mainWindow, mainApp);
-        ofRunMainLoop();
-    }else
     {
         shared_ptr<MainWindow> mainWindow = std::make_shared<MainWindow>();
         mainWindow->create("Main Window", nullptr, nullptr);
