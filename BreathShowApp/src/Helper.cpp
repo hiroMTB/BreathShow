3ea4216d74f9d64bdffe267e6ec7400288aacbe8
matoba hiroshi <matobahiroshi@gmail.com>
2023-11-29 11:22:53 +0100

    add helper

---
+++ BreathShowApp/src/Helper.cpp
@@ -0,0 +1,147 @@
+//
+//  GuiHelper.cpp
+//  BreathShowApp
+//
+//  Created by Hiroshi Matoba on 29.11.23.
+//
+
+#include "ofApp.h"
+#include "Helper.h"
+#include "io.h"
+
+namespace gui{
+
+glm::vec2 mouseOffset{0};
+
+void Helper::menu(const shared_ptr<ofApp> & app) {
+    
+    if (ImGui::BeginMenuBar()){
+        if (ImGui::BeginMenu("File")){
+            
+            //if (ImGui::MenuItem("New")) {}
+            if (ImGui::MenuItem("Open", "Ctrl+O")) {
+                io::dialogueLoadProject();
+            }
+            
+            if (ImGui::MenuItem("Save", "Ctrl+S")) {
+                io::saveProject(app->currentProjectPath);
+            }
+            
+            if (ImGui::MenuItem("Save As", "Ctrl+Shift+S")) {
+                io::dialogueSaveProject();
+            }
+            
+            ImGui::EndMenu();
+        }
+        
+        ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
+        
+        if (ImGui::BeginMenu("Panel")) {
+            //ImGui::Checkbox("Demo", (bool *) &bDemoOpen.get());
+            ImGui::Checkbox("3D Scene", (bool *) &app->b3dSceneOpen.get());
+            ImGui::Checkbox("Gui Panel", (bool *) &app->bGuiOpen.get());
+            ImGui::Checkbox("Sequencer Panel", (bool *) &app->bSeqOpen.get());
+            ImGui::Checkbox("Window Manager Panel", (bool *) &app->bWindowManagerOpen.get());
+            ImGui::EndMenu();
+        }
+        
+        ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
+        ImGui::Text("%3.1f fps", ofGetFrameRate());
+        ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
+
+        ImGui::PushItemWidth(50);
+        if(ImGui::DragInt("Target fps", (int*)&app->targetFps.get(), 1, 1, 200, "%2d fps")){
+        }
+        
+        ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
+        if(ImGui::Checkbox("Play", (bool*)&app->bPlay.get())){
+            app->setPlay(app->bPlay);
+        }
+        ImGui::Spacing(); ImGui::Separator(); ImGui::Spacing();
+        if(ImGui::Checkbox("Loop", (bool*)&app->bLoop.get())){
+            app->setLoop(app->bLoop);
+        }
+        ImGui::EndMenuBar();
+    }
+}
+
+void Helper::dock(const shared_ptr<ofApp> & app) {
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+    ImGuiWindowFlags_NoMove;
+    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+    
+    static bool p_open = NULL;
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 1));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    
+    ImGui::Begin("CentralNode", &p_open, window_flags);
+    
+    ImGui::PopStyleVar();
+    ImGui::PopStyleVar(2);
+    
+    string nameSt = "MyDockspace";
+    const char *name = nameSt.c_str();
+    
+    if (ImGui::DockBuilderGetNode(ImGui::GetID(name)) == NULL) {
+        ImGuiID dockspace_id = ImGui::GetID(name);
+        ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::DockBuilderRemoveNode(dockspace_id);
+        ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags);
+        ImGuiID dock_main_id = dockspace_id;
+        ImGuiID dock_id_left = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Left, 0.20f, NULL, &dock_main_id);
+        
+        ImGui::DockBuilderDockWindow("3D Scene", dock_main_id);
+        ImGui::DockBuilderFinish(dockspace_id);
+    }
+    
+    // DockSpace
+    ImGuiIO &io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+        ImGuiID dockspace_id = ImGui::GetID(name);
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+    
+    menu(app);
+    
+    ImGui::End();
+    ImGui::PopStyleColor();
+}
+
+ofRectangle Helper::calcCameraControlArea(){
+    ImVec2 max = ImGui::GetWindowContentRegionMax();
+    ImVec2 min = ImGui::GetWindowContentRegionMin();
+    
+    // winSize includes padding and tab, so we use max - min instead
+    ImVec2 winSize;
+    winSize.x = max.x - min.x;
+    winSize.y = max.y - min.y;
+    
+    ImVec2 winPos = ImGui::GetWindowPos(); // absolute window position
+    winPos.x = winPos.x + min.x;
+    winPos.y = winPos.y + min.y;
+    
+    if(winPos.x >= 0 && winPos.y >= 0){
+        // store actual canvas start position in order to calculate mouse action properly
+        mouseOffset = vec2(min.x, min.y);
+
+        // make it smaller to avoid moving camera while draging neiboring panel
+        int margin = 3;
+        ofRectangle controlArea = ofRectangle(winPos.x+margin, winPos.y+margin, winSize.x-margin*2, winSize.y-margin*2);
+        return controlArea;
+    }
+    return ofRectangle(0, 0, 0, 0);
+}
+
+}; // namespace gui
---
+++ BreathShowApp/src/Helper.h
@@ -0,0 +1,82 @@
+//
+//  Helper.hpp
+//
+
+#pragma once
+
+class ofApp;
+
+namespace gui{
+
+class Helper{
+
+public:
+    static void menu(const shared_ptr<ofApp> & app);
+    static void dock(const shared_ptr<ofApp> & app);
+    static ofRectangle calcCameraControlArea();
+    
+    
+    /// Drags
+    inline static void DragInt( ofParameter<int> & p, float step=1){
+        int min = p.getMin();
+        int max = p.getMax();
+        ImGui::DragScalar(p.getName().c_str(), ImGuiDataType_S32, (int *) &p.get(), step, &min, &max);
+    }
+    
+    inline static void DragFloat( ofParameter<float> & p, float step=0.01f){
+        float min = p.getMin();
+        float max = p.getMax();
+        ImGui::DragScalar(p.getName().c_str(), ImGuiDataType_Float, (float *) &p.get(), step, &min, &max);
+    }
+    
+    inline static void DragFloat3( const char * name, ofParameter<glm::vec3> & p, float step=0.01f){
+        float min = p.getMin().x;
+        float max = p.getMax().x;
+        ImGui::DragFloat3(name, (float *) &p.get().x, step, min, max);
+    }
+    
+    inline static void DragDouble( ofParameter<double> & p, float step=0.01f){
+        double min = p.getMin();
+        double max = p.getMax();
+        ImGui::DragScalar(p.getName().c_str(), ImGuiDataType_Double, (double *) &p.get(), step, &min, &max);
+    }
+    
+    inline static void Combo( ofParameter<int> & p, const vector<string> & items){
+        //ImGui::Combo(p.getName().c_str(), (int*&)p.get(), (char*)&list[0]);
+        // Using the generic BeginCombo() API, you have full control over how to display the combo contents.
+        // (your selection data could be an index, a pointer to the object, an id for the object, a flag intrusively
+        // stored in the object itself, etc.)
+        const string combo_preview_value = items[p];
+        ImGuiComboFlags_ flags = ImGuiComboFlags_None;
+        /// ImGuiComboFlags_PopupAlignLeft
+        /// ImGuiComboFlags_HeightSmall
+        /// ImGuiComboFlags_HeightRegular
+        /// ImGuiComboFlags_HeightLarge
+        /// ImGuiComboFlags_HeightLargest
+        /// ImGuiComboFlags_NoArrowButton
+        /// ImGuiComboFlags_NoPreview
+        /// ImGuiComboFlags_HeightMask_
+        if (ImGui::BeginCombo(p.getName().c_str(), combo_preview_value.c_str(), flags)){
+            for (int n = 0; n < items.size(); n++){
+                const bool is_selected = (p == n);
+                if (ImGui::Selectable(items[n].c_str(), is_selected))
+                    p = n;
+                
+                // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                if (is_selected)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndCombo();
+        }
+    }
+    
+    inline static void drawMsgLogger(const std::deque<string> & queue){
+        if(queue.size() !=0 ){
+            for(const string & s : queue){
+                ImGui::Text("%s", s.c_str());
+            }
+        }
+    }
+};
+
+};
